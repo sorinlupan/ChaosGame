7b7ce0f122461a8c11345b6d17c343e512756b72
Your Name <you@example.com>
2024-11-19 12:17:06 -0800

    commit from Ubuntu

---
+++ code/ChaosGame.cpp
@@ -0,0 +1,120 @@
+// Include important C++ libraries here
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+//Make the code easier to type with "using namespace"
+using namespace sf;
+using namespace std;
+
+int main()
+{
+	// Create a video mode object
+	VideoMode vm(1920, 1080);
+	// Create and open a window for the game
+	RenderWindow window(vm, "Chaos Game!!", Style::Default);
+
+	sf::Font font;
+	if (!font.loadFromFile("Arial.ttf")) {
+		cout << "could not load font " << endl;
+		return -1;
+	}
+
+	sf::Text text;
+	text.setFont(font);
+	text.setString("click any three points on screen");
+	text.setCharacterSize(24);
+	text.setFillColor(Color::Green);
+	text.setPosition(10, 10);
+
+
+	vector<Vector2f> vertices;
+	vector<Vector2f> points;
+
+	while (window.isOpen())
+	{
+		/*
+		****************************************
+		Handle the players input
+		****************************************
+		*/
+		Event event;
+		while (window.pollEvent(event))
+		{
+		    if (event.type == Event::Closed)
+		    {
+					// Quit the game when the window is closed
+					window.close();
+		    }
+		    if (event.type == sf::Event::MouseButtonPressed)
+		    {
+				if (event.mouseButton.button == sf::Mouse::Left)
+	
+				{
+					std::cout << "the left button was pressed" << std::endl;
+			        std::cout << "mouse x: " << event.mouseButton.x << std::endl;
+			        std::cout << "mouse y: " << event.mouseButton.y << std::endl;
+	
+			        if(vertices.size() < 3)
+			        {   
+						vertices.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+					}
+					else if(points.size() == 0)
+			        {
+				///fourth click
+				///push back to points vector
+						points.push_back(Vector2f(event.mouseButton.x, event.mouseButton.y));
+			        }
+				}
+		    }
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+		/*
+		****************************************
+		Update
+		****************************************
+		*/
+	
+		if(points.size() > 0)
+		{
+		    ///generate more point(s)
+		    ///select random vertex
+		    ///calculate midpoint between random vertex and the last point in the vector
+		    ///push back the newly generated coord.
+			for (int i = 0; i < 60; i++) {
+				Vector2f randomVertex = vertices[rand() % 3];
+				Vector2f lastPoint = points.back();
+				Vector2f midPoint((lastPoint.x + randomVertex.x) / 2, (lastPoint.y + randomVertex.y) / 2);
+				points.push_back(midPoint);
+			}
+		}
+	
+		/*
+		****************************************
+		Draw
+		****************************************
+		*/
+		window.clear();
+		for(int i = 0; i < vertices.size(); i++)
+		{
+		    RectangleShape rect(Vector2f(10,10));
+		    rect.setPosition(Vector2f(vertices[i].x, vertices[i].y));
+		    rect.setFillColor(Color::Blue);
+		    window.draw(rect);
+		}
+		for (size_t i = 0; i < points.size(); i++) 
+		{
+			CircleShape circle;
+			circle.setRadius(2);
+			circle.setPosition(points[i]);
+			circle.setFillColor(Color::Red);
+			window.draw(circle);
+		}
+		window.display();
+	}
+}
